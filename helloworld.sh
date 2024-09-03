ea0b51428d4e396f49dca8c5559eecb0eca64c99
Alejandro Ponce <alexponce36@hotmail.com>
2024-09-02 20:52:57 -0600

    metiendo entradas a archivos

--- helloworld.sh
+++ helloworld.sh
@@ -1,2 +1,8 @@
 #! /bin/bash
-echo "Hello world!"
+echo "Hello world!" > hola.txt
+
+# Agregar lineas escritas desde terminal, pero sobre escribe
+# cat > cat.txt
+
+# Agregar lineas escritas desde termian pero lo agrega a lo que ya se tiene
+cat >> cat.txt

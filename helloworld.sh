f6bb104c0ba47fdb0587eb789b731b434ef0ee6b
Alejandro Ponce <alexponce36@hotmail.com>
2024-09-02 20:55:25 -0600

    comentarios

--- helloworld.sh
+++ helloworld.sh
@@ -6,3 +6,10 @@ echo "Hello world!" > hola.txt
 
 # Agregar lineas escritas desde termian pero lo agrega a lo que ya se tiene
 cat >> cat.txt
+
+# Comentarios sencillos
+
+: 'Esto es un comentario de muchas lineas
+Para esto se usan las comillas simples
+Con : al inicio de las lineas
+'

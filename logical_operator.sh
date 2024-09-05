f157ca20a79102ebc57b10e17b2cd4a667f5dc3c
Alejandro Ponce <alexponce36@hotmail.com>
2024-09-04 20:43:55 -0600

    operadores logicos

---
+++ logical_operator.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+age=17
+
+# 18 < age < 40
+if [ $age -gt 18 ] && [ $age -lt 40 ]
+then
+    echo "Edad valida"
+else
+    echo "Edad no valida"
+fi
+
+# 18 < age < 40
+if [[ $age -gt 18 && $age -lt 40 ]]
+then
+    echo "Edad valida"
+else
+    echo "Edad no valida"
+fi
+
+if [[ $age -gt 18 || $age -lt 40 ]]
+then
+    echo "Edad valida"
+else
+    echo "Edad no valida"
+fi
+
+if [ $age -gt 18 ] || [ $age -lt 40 ]
+then
+    echo "Edad valida"
+else
+    echo "Edad no valida"
+fi
